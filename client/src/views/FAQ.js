7c9ac64f5c4de9214eee4ea4b771b96a1195684f
Nick Ray <nickrray@gmail.com>
2020-10-07 20:49:34 -0500

    :construction: Fetch FAQs on component mount and store in state

--- client/src/views/FAQ.js
+++ client/src/views/FAQ.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import api from 'api';
+
+const faqAPI = api('faqs');
+
+export const FAQ = () => {
+  const [faqs, setFaqs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setFaqs(await faqAPI.show());
+    })()
+  }, []);
+
 export const FAQ = () => {
   return (
     <div>
