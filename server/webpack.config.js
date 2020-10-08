97a04742ed6c529223122a71341cd796c64c0400
Nick Ray <nickrray@gmail.com>
2020-10-08 09:56:58 -0500

    :wrench: Change webpack config
    Attempting to successfully deploy to Heroku

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,14 +1,8 @@
-const path = require('path');
-
 module.exports = {
   target: 'node',
   entry: './server.js',
   mode: 'development',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   resolve: {
-    modules: [path.resolve(__dirname, 'server'), 'node_modules'],
+    modules: ['server', 'node_modules'],
   },
 };
