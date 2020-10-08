8acef14d166ddb69f7d65d3c38f6c099dee1cc10
Nick Ray <nickrray@gmail.com>
2020-10-07 20:56:59 -0500

    :sparkles: Add Link to FAQ page in header and style it
    Fix link path for faq in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,10 @@ export const Header = () => {
   return (
     <div class="top-nav flex flex--align-center flex--justify-between">
       <h1 className="title">Nick's FAQs</h1>
-      <Link className="btn outline-btn secondary-fill" to="/">Login</Link>
+      <div className="top-nav-right">
+        <Link className="med-text text-link" to="/faq">FAQs</Link>
+        <Link className="btn outline-btn secondary-fill" to="/">Login</Link>
+      </div>
     </div>
   )
 }
