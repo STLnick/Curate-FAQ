f13a6ea3d348a674235ca36c345edcd8fb8111d2
Nick Ray <nickrray@gmail.com>
2020-10-08 09:21:06 -0500

    :rotating_light: Change class to className

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer class="flex flex--align-center flex--justify-between">
+    <footer className="flex flex--align-center flex--justify-between">
       <span>Checkout all the code on <a href="https://github.com/STLnick/Curate-FAQ">GitHub</a></span>
       Developed by Nick Ray
     </footer>
