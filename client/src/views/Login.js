d6085cd050a706b729f33bb7d2b55c0f594011ce
Nick Ray <nickrray@gmail.com>
2020-10-07 21:42:54 -0500

    :sparkles: Login a user - store in state and redirect to /faq

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { UserContext } from 'UserContext';
+
+import api from 'api';
+
+const userAPI = api('users');
 
 export const Login = () => {
+  const history = useHistory();
+  const [error, setError] = useState('');
+  const { setUser } = useContext(UserContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // Get info from fields
+    const userInfo = Array.from(e.target.elements).filter(el => el.id).reduce((acc, el) => {
+      acc[el.id] = el.value;
+      return acc;
+    }, {})
+
+    console.log('USERINFO: ', userInfo);
+
+    // Try to find in database
+    const response = await userAPI.login(userInfo);
+
+    // If successful, setUser and redirect to /faq else display error
+    if (response) {
+      setUser(response);
+      history.push('/faq')
+    } else {
+      setError('Email or Password is incorrect. Please try again.')
+    }
+  }
+
   return (
     <>
       <h2 className="heading">Login</h2>
-      <form class="login-form flex flex--column">
+      <form className="login-form flex flex--column" onSubmit={(e) => handleLogin(e)}>
         <label className="med-text" htmlFor="email">Email</label>
         <input className="input" id="email" type="email" />
         <label className="med-text" htmlFor="password">Password</label>
         <input className="input" id="password" type="password" />
         <button className="btn cta-btn" type="submit">Login</button>
       </form>
+      <span className="error">{error}</span>
     </>
   )
 }
