1aa23d9cad71abd5c4c77438c2a2e511ffa0f210
Nick Ray <nickrray@gmail.com>
2020-10-07 19:09:11 -0500

    :construction: Setup imports and use of routes to be created

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import cors from 'cors';
 import express from 'express';
 
+//import faqs from './routes/faqs';
+import users from './routes/users';
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from Express!');
-});
+//app.use('/faqs', faqs);
+app.use('/users', users);
 
 app.listen(5000, () => {
   console.log('Server is running on Port 5000!');
