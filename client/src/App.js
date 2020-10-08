70656ed36ef377c4af89e0425531228dc49f0b07
Nick Ray <nickrray@gmail.com>
2020-10-07 21:42:33 -0500

    :construction: Setup UserContext provider and memo

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { Header } from './components';
 import { FAQ, Login, NotFound } from './views';
+import { UserContext } from './UserContext';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const providerValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
     <>
-      <Header />
-      <div className="container flex flex--column flex--align-center flex--justify-center">
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route exact path="/faq">
-            <FAQ />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
+      <UserContext.Provider value={providerValue}>
+        <Header />
+        <div className="container flex flex--column flex--align-center flex--justify-center">
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/faq">
+              <FAQ />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </UserContext.Provider>
     </>
   )
 }
