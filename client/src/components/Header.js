0e462dfeb244abcacb65ef30164e190131f69a1f
Nick Ray <nickrray@gmail.com>
2020-10-07 21:41:42 -0500

    :sparkles: Render Login or Logout depending on if someone is logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { UserContext } from 'UserContext';
 
 export const Header = () => {
+  const history = useHistory();
+  const { user, setUser } = useContext(UserContext);
+
+  const handleLogout = () => {
+    setUser(null);
+    history.push('/');
+  }
+
   return (
-    <div class="top-nav flex flex--align-center flex--justify-between">
+    <div className="top-nav flex flex--align-center flex--justify-between">
       <h1 className="title">Nick's FAQs</h1>
       <div className="top-nav-right">
         <Link className="med-text text-link" to="/faq">FAQs</Link>
-        <Link className="btn outline-btn secondary-fill" to="/">Login</Link>
+        {user
+          ? <button
+            className="btn outline-btn secondary-fill"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+          : <Link className="btn outline-btn secondary-fill" to="/">Login</Link>}
       </div>
     </div>
   )
