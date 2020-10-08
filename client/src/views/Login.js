af8568e6bfa6d3ec73dfca40b212f0f16158fe7d
Nick Ray <nickrray@gmail.com>
2020-10-07 20:36:35 -0500

    :construction: Setup structure of Login form and view

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 export const Login = () => {
   return (
-    <form>
-      <label htmlFor="email">Email</label>
-      <input id="email" type="email" />
-      <label htmlFor="password">Password</label>
-      <input id="password" type="password" />
-      <button type="submit">Login</button>
-    </form>
+    <>
+      <h2 className="heading">Login</h2>
+      <form class="login-form flex flex--column">
+        <label className="med-text" htmlFor="email">Email</label>
+        <input className="input" id="email" type="email" />
+        <label className="med-text" htmlFor="password">Password</label>
+        <input className="input" id="password" type="password" />
+        <button className="btn cta-btn" type="submit">Login</button>
+      </form>
+    </>
   )
 }
